4cbe81c015c38f8c44c2440a8c3ec1bd52890413
hschafer2017 <hschafer@umich.edu>
2020-05-30 14:55:41 +0100

    created dogs model and serializers with foriegn keys to breed and colors

--- dogs/models.py
+++ dogs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Breed(models.Model):
     breed_name = models.CharField(max_length=200)
@@ -11,10 +11,43 @@ class Breed(models.Model):
     avg_weight = models.CharField(max_length=100)
     avg_height = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.breed_name
+
+
+class Dog(models.Model):
+    GENDERS = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
+
+    name = models.CharField(max_length=200)
+    age = models.CharField(max_length=100)
+    gender = models.CharField(choices=GENDERS, max_length=15)
+    description = models.TextField()
+    spayed_neutered = models.BooleanField(null=True)
+    image = models.ImageField(upload_to='')
+    uploaded_date = models.DateField(
+        blank=True, null=True, default=timezone.now)
+    breed = models.ForeignKey(
+        'Breed', on_delete=models.CASCADE, related_name='breed')
+
+    class Meta:
+        unique_together = ('breed', 'name')
+        ordering = ['breed']
+
+    def __str__(self):
+        return f'{self.name} - {self.breed.breed_name}'
+
 
 class Color(models.Model):
     breed_name = models.ManyToManyField(
         Breed,
+        related_name='colors',
+        related_query_name='colors',
+    )
+    dog_name = models.ManyToManyField(
+        Dog,
         related_name='colors',
         related_query_name='colors',
     )
--- dogs/serializers.py
+++ dogs/serializers.py
@@ -1,19 +1,50 @@
 from rest_framework import fields, serializers
-from .models import Breed, Color
+from .models import Breed, Color, Dog
 
 
+GENDERS = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+)
 
+class ChoiceField(serializers.ChoiceField):
 
-class BreedSerializer(serializers.ModelSerializer):
-    colors = serializers.SlugRelatedField(many=True, slug_field="color", queryset=Color.objects.all())
-
-    class Meta:
-        model = Breed
-        fields = ('breed_name', 'description', 'history', 'origin', 'colors',
-                  'temperament', 'lifespan', 'avg_weight', 'avg_height')
+    def to_representation(self, obj):
+        return self._choices[obj]
 
 
 class ColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Color
         fields = ('color',)
+
+
+class DogSerializer(serializers.ModelSerializer):
+    gender = ChoiceField(choices=Dog.GENDERS)
+    colors = serializers.SlugRelatedField(
+        many=True, slug_field="color", queryset=Color.objects.all())
+    breed = serializers.PrimaryKeyRelatedField(queryset=Breed.objects.all())
+
+    class Meta:
+        model = Dog
+        fields = ('name', 'age', 'description', 'spayed_neutered',
+                  'uploaded_date', 'breed', 'colors', 'image', 'gender')
+
+    def get_gender(self, obj):
+        return obj.get_gender_display()
+
+    def to_representation(self, instance):
+        rep = super(DogSerializer, self).to_representation(instance)
+        rep['breed'] = instance.breed.breed_name
+        return rep
+
+
+class BreedSerializer(serializers.ModelSerializer):
+    colors = serializers.SlugRelatedField(
+        many=True, slug_field="color", queryset=Color.objects.all())
+    dogs = DogSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Breed
+        fields = ('breed_name', 'description', 'history', 'origin', 'colors',
+                  'temperament', 'lifespan', 'avg_weight', 'avg_height', 'dogs')
