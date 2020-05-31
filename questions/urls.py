ad05b8c8e18aafa6a626920b077236c758765550
hschafer2017 <hschafer@umich.edu>
2020-05-31 16:18:42 +0100

    added detail views and ordered list views by certain field for questions, tags, answers

--- questions/urls.py
+++ questions/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('api/question', views.QuestionListCreate.as_view()),
+    path('api/question/<int:pk>', views.QuestionDetail.as_view()),
     path('api/tags', views.TagListCreate.as_view()),
+    path('api/tags/<int:pk>', views.TagDetail.as_view()),
+    path('api/answer', views.AnswerListCreate.as_view()),
+    path('api/answer/<int:pk>', views.AnswerDetail.as_view()),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
-from .models import Question, Tags
-from .serializers import QuestionSerializer, TagsSerializer
+from .models import Question, Tags, Answer
+from .serializers import QuestionSerializer, TagsSerializer, AnswerSerializer
 from rest_framework import generics
 
 
 class QuestionListCreate(generics.ListCreateAPIView):
+    queryset = Question.objects.order_by('-date')
+    serializer_class = QuestionSerializer
+
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
 
 class TagListCreate(generics.ListCreateAPIView):
+    queryset = Tags.objects.order_by('tag')
+    serializer_class = TagsSerializer
+
+
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tags.objects.all()
     serializer_class = TagsSerializer
+
+
+class AnswerListCreate(generics.ListCreateAPIView):
+    queryset = Answer.objects.order_by('-date')
+    serializer_class = AnswerSerializer
+
+
+class AnswerDetail(generics.ListCreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
