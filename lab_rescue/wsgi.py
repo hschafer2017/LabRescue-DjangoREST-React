b4fa7cbc361a2f6a86ace8fe187aae65e73c6d34
hschafer2017 <hschafer@umich.edu>
2020-05-31 12:16:59 +0100

    changed from pitbull_rescue to lab_rescue

--- lab_rescue/wsgi.py
+++ lab_rescue/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pitbull_rescue project.
+WSGI config for lab_rescue project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pitbull_rescue.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab_rescue.settings')
 
 application = get_wsgi_application()
