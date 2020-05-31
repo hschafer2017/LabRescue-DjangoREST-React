12c7249cf411d7c2150cc9ebe0235316185989a7
hschafer2017 <hschafer@umich.edu>
2020-05-31 20:50:42 +0100

    basic custom user auth model, form, and admin configuration

--- lab_rescue/settings.py
+++ lab_rescue/settings.py
@@ -41,10 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dogs.apps.DogsConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'dogs.apps.DogsConfig',
     'frontend',
     'questions.apps.QuestionsConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # REST_FRAMEWORK = {
