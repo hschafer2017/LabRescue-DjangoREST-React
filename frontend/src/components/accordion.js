ff8264f2c614695e9844703734dde69738244dcb
hschafer2017 <hschafer@umich.edu>
2020-06-18 21:19:35 +0100

    added initial questions data into accordion with each accordion collapsing properly

--- frontend/src/components/accordion.js
+++ frontend/src/components/accordion.js
@@ -9,50 +9,44 @@ class QuestionAccordion extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          questions: [],
-          loaded: false,
-          placeholder: "Loading"
+            questions: [],
+            loaded: false,
+            placeholder: "Loading"
         };
-      }
+    }
     
-      componentDidMount() {
+    componentDidMount() {
         fetch("api/question")
         .then(response => {
-          if (response.status > 400) {
-            return response;
-          };
-          return response.json();
-          })
+            if (response.status > 400) {
+                return response;
+            };
+            return response.json();
+            })
         .then(allQuestions => {
-          this.setState({
-            questions: allQuestions
-          })
+            this.setState({
+                questions: allQuestions
+            })
         });
-      }
+    }
 
     render () {
         return (
             <Accordion defaultActiveKey="0">
-                <Card>
-                    <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        Click me!
-                    </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0">
-                    <Card.Body>Hello! I'm the body</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-                <Card>
-                    <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                        Click me!
-                    </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="1">
-                    <Card.Body>Hello! I'm another body</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
+                {this.state.questions.map((question, index) => {
+                    return (
+                        <Card key={index}>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey={index}>
+                                {question.title}
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey={index}>
+                            <Card.Body>{question.content}</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    )
+                })}
             </Accordion>
         )
     };
