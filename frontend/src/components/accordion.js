c03eebbd46433312502e790def02037183172854
hschafer2017 <hschafer@umich.edu>
2020-06-25 22:30:30 +0100

    formatted answers into accordion within each question accordion

--- frontend/src/components/accordion.js
+++ frontend/src/components/accordion.js
@@ -66,22 +66,44 @@ class QuestionAccordion extends Component {
                                     <Container>
                                         <Row className="flex-wrap">
                                             <Col xs={8} sm={8} md={8} lg={8} className="justify-content-center my-2">
-                                                <p className='float-left font-italic font-weight-light'>{question.author} &mdash; </p>
+                                                <p className='float-left font-italic font-weight-light'>{question.author} &mdash;</p>
                                             </Col>
                                             <Col xs={4} sm={4} md={4} lg={4} className="justify-content-center my-2">
                                                 <p className='float-right font-italic font-weight-light'>{question.date}</p>
                                             </Col>
                                         </Row>
                                         <p className="mb-3">{question.content}</p>
-                                        <h6 className='font-weight-bold font-italic my-2'>Answers:</h6>
-                                        {question.answers.map((answer, index) => {
-                                            return (
-                                                <span key={index}>
-                                                    <p>{answer.content}</p>
-                                                    <p className='float-right font-italic font-weight-light'>&mdash; {answer.author} on {answer.date}:</p>
-                                                </span>
-                                            )
-                                        })}
+                                        <Accordion defaultActiveKey="0">
+                                            <Card key={index} style={{border: 'none', backgroundColor: '#fafafa'}}>
+                                                <Card.Header style={{backgroundColor: '#fafafa', borderBottom: 'none'}}>
+                                                    <Row className="flex-wrap">
+                                                        <Col xs={12} sm={12} md={12} lg={12} className="justify-content-center">
+                                                            <p>Answers
+                                                                <Accordion.Toggle as={Button} variant="link" eventKey={index}>
+                                                                <FontAwesomeIcon style={{color: '#5C5E60'}} icon={faPlus} />
+                                                                </Accordion.Toggle>
+                                                            </p>
+                                                        </Col>
+                                                    </Row>
+                                                </Card.Header>
+                                                <Accordion.Collapse eventKey={index}>
+                                                    <Card.Body style={{backgroundColor: '#fafafa', borderBottom: 'none'}}>
+                                                        <Container>
+                                                            {question.answers.map((answer, index) => {
+                                                                return (
+                                                                    <Row className="flex-wrap">
+                                                                        <span key={index}>
+                                                                            <p>{answer.content}</p>
+                                                                            <p className='float-right font-italic font-weight-light'>&mdash; {answer.author} on {answer.date}</p>
+                                                                        </span>
+                                                                    </Row>
+                                                                )
+                                                            })}
+                                                        </Container>
+                                                    </Card.Body>
+                                                </Accordion.Collapse>
+                                            </Card>
+                                        </Accordion>
                                     </Container>
                                 </Card.Body>
                             </Accordion.Collapse>
