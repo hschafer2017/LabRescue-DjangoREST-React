0e8a3e6dd7d8ea83f0f131d0f8bb9d7bfa1a4725
hschafer2017 <hschafer@umich.edu>
2020-06-25 22:13:54 +0100

    brought in answers and started basic styling of question formatting

--- frontend/src/components/accordion.js
+++ frontend/src/components/accordion.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import { render } from "react-dom";
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const questionStyles = {
+    backgroundColor: '#FAFAFA',
+    color: '#5C5E60',
+    fontWeight: '500',
+}
 
 class QuestionAccordion extends Component {
     constructor(props) {
@@ -32,22 +44,52 @@ class QuestionAccordion extends Component {
 
     render () {
         return (
-            <Accordion defaultActiveKey="0">
-                {this.state.questions.map((question, index) => {
-                    return (
-                        <Card key={index}>
-                        <Card.Header>
-                            <Accordion.Toggle as={Button} variant="link" eventKey={index}>
-                                {question.title}
-                            </Accordion.Toggle>
-                        </Card.Header>
-                        <Accordion.Collapse eventKey={index}>
-                            <Card.Body>{question.content}</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                    )
-                })}
-            </Accordion>
+            <Container style={questionStyles}>
+                <h2 className="my-4">Ask Questions.</h2>
+                <Accordion defaultActiveKey="0">
+                    {this.state.questions.map((question, index) => {
+                        return (
+                            <Card key={index} style={{border: 'none', backgroundColor: '#fafafa'}}>
+                            <Card.Header style={{backgroundColor: '#fafafa', borderBottom: 'none'}}>
+                                <Row className="flex-wrap">
+                                    <Col xs={12} sm={12} md={12} lg={12} className="justify-content-center">
+                                        <p>{question.title}
+                                            <Accordion.Toggle as={Button} variant="link" eventKey={index}>
+                                            <FontAwesomeIcon style={{color: '#5C5E60'}} icon={faPlus} />
+                                            </Accordion.Toggle>
+                                        </p>
+                                    </Col>
+                                </Row>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey={index}>
+                                <Card.Body style={{backgroundColor: '#fafafa', borderBottom: 'none'}}>
+                                    <Container>
+                                        <Row className="flex-wrap">
+                                            <Col xs={8} sm={8} md={8} lg={8} className="justify-content-center my-2">
+                                                <p className='float-left font-italic font-weight-light'>{question.author} &mdash; </p>
+                                            </Col>
+                                            <Col xs={4} sm={4} md={4} lg={4} className="justify-content-center my-2">
+                                                <p className='float-right font-italic font-weight-light'>{question.date}</p>
+                                            </Col>
+                                        </Row>
+                                        <p className="mb-3">{question.content}</p>
+                                        <h6 className='font-weight-bold font-italic my-2'>Answers:</h6>
+                                        {question.answers.map((answer, index) => {
+                                            return (
+                                                <span key={index}>
+                                                    <p>{answer.content}</p>
+                                                    <p className='float-right font-italic font-weight-light'>&mdash; {answer.author} on {answer.date}:</p>
+                                                </span>
+                                            )
+                                        })}
+                                    </Container>
+                                </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        )
+                    })}
+                </Accordion>
+            </Container>
         )
     };
 };
