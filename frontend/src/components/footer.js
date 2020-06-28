4e8b8de047b01f8fcccae2831d4c7f9a665a6b13
hschafer2017 <hschafer@umich.edu>
2020-06-28 19:13:09 +0100

    styling for footer with positioning and social media icons

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import { render } from "react-dom";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
 
 class Footer extends Component {
     render () {
         return (
-            <footer className="mt-4 mb-2 w-100 text-center">
-                <h6>The Lab Rescue</h6>
+            <footer className="mt-4 w-100 text-center pt-3">
+                <h5 className="font-weight-bold">The Lab Rescue</h5>
                 <p>See Dogs. Ask Questions. Be Educated.</p>
-                <p>Some social media icons</p>
+                <Row>
+                    <Col xs={{ span: 12}} sm={{span: 10, offset: 2}} md={{ span: 6, offset: 3 }} lg={{span:4, offset: 4}}>
+                    <span className="fa-layers fa-2x mx-3">
+                        <FontAwesomeIcon icon={faCircle} size="lg" style={{color: '#5C5E60'}} />
+                        <FontAwesomeIcon style={{color: '#fafafa'}}  icon={faFacebookF} className="mx-2" />
+                    </span>
+                    <span className="fa-layers fa-2x mx-3">
+                        <FontAwesomeIcon icon={faCircle} size='lg' style={{color: '#5C5E60'}} />
+                        <FontAwesomeIcon style={{color: '#fafafa'}} size='sm' icon={faInstagram} className="mx-2"/>
+                    </span>
+                    <span className="fa-layers fa-2x mx-3">
+                        <FontAwesomeIcon icon={faCircle} size='lg' style={{color: '#5C5E60'}} />
+                        <FontAwesomeIcon style={{color: '#fafafa'}} size='sm' icon={faTwitter} className="mx-2"/>
+                    </span>
+                    <span className="fa-layers fa-2x mx-3">
+                        <FontAwesomeIcon icon={faCircle} size='lg' style={{color: '#5C5E60'}} />
+                        <FontAwesomeIcon style={{color: '#fafafa'}} size='sm' icon={faEnvelope} className="mx-2"/>
+                    </span>
+                    </Col>
+                </Row>
             </footer>
         )
     }
